af3d0d6b956294bf748961767f1a00034f563d63
CodeBunk <asreddy5270@gmail.com>
2025-01-03 23:20:00 +0530

    Added changes in the projects

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -7,17 +7,53 @@ const Cards = () => {
   const Data = [
     {
       src: "/KingQueen.png",
-      url: "https://www.behance.net/gallery/156976797/KingzQueenZ-Rummy",
+      url: "/kingzqueenz",
       title: "Kingz Queenz",
       description:
         "An online rummy game designed to increase the interaction and engagment with the players or friends group",
     },
     {
+      src: "/KingQueen.png",
+      url: "/leacraft",
+      title: "Leafcraft",
+      description:
+        "An online rummy game designed to increase the interaction and engagment with the players or friends group",
+    },
+    {
+      src: "/KingQueen.png",
+      url: "/wishtronics",
+      title: "wishtronics",
+      description:
+        "An online rummy game designed to increase the interaction and engagment with the players or friends group",
+    },
+    // {
+    //   src: "/KingQueen.png",
+    //   url: "https://www.behance.net/gallery/156976797/KingzQueenZ-Rummy",
+    //   title: "Leafcraft",
+    //   description:
+    //     "An online rummy game designed to increase the interaction and engagment with the players or friends group",
+    // },
+
+    {
       src: "/OneImmersive.png",
-      url: "https://www.behance.net/gallery/154949507/OI-Branding-Guidelines",
+      url: "/one-immersive",
       title: "One Immersive",
       description:
         "building a brand narrative that resonates with its audience while maintaining aesthetic consistency and brand integrity.",
+    },
+    // {
+    //   src: "/OneImmersive.png",
+    //   url: "/one-immersive",
+    //   title: "One Immersive",
+    //   description:
+    //     "building a brand narrative that resonates with its audience while maintaining aesthetic consistency and brand integrity.",
+    // },
+    {
+      src: "/KingQueen.png",
+      url: "https://www.behance.net/gallery/156976797/KingzQueenZ-Rummy",
+      title: "Travana",
+      description:
+        "An online rummy game designed to increase the interaction and engagment with the players or friends group",
     },
     {
       src: "/travia.png",
@@ -27,12 +63,31 @@ const Cards = () => {
         "A travel community application made to plan all the travel activities at a place.",
     },
   ];
+
+  // const List = [
+  //   {
+  //     Title: "Wishtronics",
+  //     URI: "wishtronics",
+  //   },
+  //   {
+  //     Title: "Digital Marketing Collabs",
+  //     URI: "collabs",
+  //   },
+  //   {
+  //     Title: "Travana",
+  //     URI: "travana",
+  //   },
+  //   {
+  //     Title: "Technovation",
+  //     URI: "technovation",
+  //   },
+  // ];
   return (
     <div className=" w-full lg:px-20 px-4 text-white flex flex-col gap-10 max-md:pt-20 ">
       <div className=" lg:text-5xl text-3xl font-times max-md:text-center">
         Featured Projects
       </div>
-      <div className="grid md:grid-cols-2 grid-cols-1 md:gap-10 gap-12 ">
+      <div className="grid md:grid-cols-3 grid-cols-1 md:gap-10 gap-12 ">
         {Data.map((props: any) => (
           <a
             className="flex flex-col gap-4 hover:scale-[103%] transition-all duration-300 cursor-pointer  "
@@ -50,8 +105,8 @@ const Cards = () => {
             <div className="text-white font-poppins">{props.description}</div>
           </a>
         ))}
-        <div
-          className="flex flex-col gap-4  text-black hover:text-white transition-all duration-300 cursor-pointer   "
+        {/* <div
+          className="flex flex-col gap-4 hidden  text-black hover:text-white transition-all duration-300 cursor-pointer   "
           onClick={() => location("/projects")}
         >
           <div className=" aspect-video hover:scale-[103%] transition-all duration-300 rounded-3xl bg-[#0027F1] flex text-center items-center">
@@ -61,12 +116,12 @@ const Cards = () => {
             </p>
           </div>
           <div className=" md:text-inherit text-white font-times text-3xl font-normal ">
-            click to go to projects page
+            click to go to Behance page
           </div>
           <div className=" md:text-inherit text-white font-poppins">
             find the list of all the notable projects{" "}
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
