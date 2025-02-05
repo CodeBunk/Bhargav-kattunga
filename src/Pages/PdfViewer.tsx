023a50cd5ced2c36dd5e0129906cc47fd08f0fff
Srinivas <abcom@Abcoms-MacBook-Air.local>
2025-02-05 14:32:29 +0530

    Added new updated reusme

--- src/Pages/PdfViewer.tsx
+++ src/Pages/PdfViewer.tsx
@@ -1,13 +1,14 @@
 // src/components/PdfViewer.tsx
 import React from "react";
 import Navbar from "../components/Navbar";
+import Resume from "../../public/Bhargav.pdf"
 
 const PdfViewer = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-black">
       <Navbar />
       <embed
-        src="/BhargavResume.pdf"
+        src={Resume}
         width="100%"
         height="100%"
         className="bg-black pt-[112px]"
