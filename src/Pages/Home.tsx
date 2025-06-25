363e5455e78bc20b630fd223891e9140dbafd030
CodeBunk <asreddy5270@gmail.com>
2025-06-25 19:48:37 +0530

    Added changes in the home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Hero from "../components/Hero";
 import Cards from "../components/Cards";
 import Navbar from "../components/Navbar";
@@ -16,12 +16,35 @@ import Button from "../components/Atoms/Button";
 const Home = ({ muted }) => {
 
   const Data = [< Home1 value={muted} />, < Home2 />, < Home3 />, < Home4 />, <Home5 />]
-  return (
-    <div className=' w-full h-full overflow-x-clip '>
+  console.log(window.scrollY, "screen")
 
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const percent = docHeight > 0 ? (scrollTop / docHeight) * 100 : 0;
+      setScrollProgress(percent);
+    };
+    +
+      // (you already had an empty useEffect here)
+      window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div id="home" className=' w-full h-full overflow-x-clip '>
+      <div className=" z-[100] fixed w-screen h-1 md:top-16 top-[73px] left-0 ">
+        {/* <div className={`bg-black h-full  w-[50%] rounded`}></div> */}
+        <div
+          className="bg-black/50 h-full rounded"
+          style={{ width: `${scrollProgress}%` }}
+        />
+      </div>
 
       {Data?.map((data: any, index) => (
-        <div key={index} className='  sticky top-0    w-screen h-[300vh] bg-white   '>
+        <div key={index} className='  sticky top-0    w-screen h-[150vh] bg-white   '>
           <div className='  sticky top-0  w-screen h-screen  flex items-center justify-center  '>
             {data}
           </div>
